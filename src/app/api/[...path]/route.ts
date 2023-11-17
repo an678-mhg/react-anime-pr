241d4eb95a30ecda418178ae4550b6570a41a0d7
An Nguyen <an.nguyen@vnetwork.vn>
2023-11-17 14:26:17 +0700

    chore: push code

--- src/app/api/[...path]/route.ts
+++ src/app/api/[...path]/route.ts
@@ -1,14 +1,14 @@
 import { server } from "@/shared/fetch";
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL as string;
 
-export async function GET(request: Request) {
+export async function GET(request: NextRequest) {
   const pathname = request.url?.split(`${BASE_URL}/api/`)[1];
   const response = await server?.get(pathname);
   return NextResponse.json(response?.data);
 }
 
-export async function POST(request: Request) {
+export async function POST(request: NextRequest) {
   const pathname = request.url?.split(`${BASE_URL}/api/`)[1];
   const data = await request.json();
   const response = await server?.post(pathname, data);
